20a2da32757d646c328bb78bee8961efff69ab3c
Robert Peek <robert@delta-net.co.uk>
2021-01-11 16:08:06 +0000

    Formatted code, split less into item and main, and testing fixes

--- js/adapt-hotspot-hotspotItemView.js
+++ js/adapt-hotspot-hotspotItemView.js
@@ -1,170 +1,169 @@
 define([
-    'core/js/adapt',
-    'core/js/views/menuItemView'
+  'core/js/adapt',
+  'core/js/views/menuItemView'
 ], function(Adapt, MenuItemView) {
 
-    var HotspotItemView = MenuItemView.extend({
+  var HotspotItemView = MenuItemView.extend({
 
-        events: {
-            'click .menu-item-hotspot':'showDetails',
-            'click .menu-item-graphic-button':'showDetails',
-            'click .menu-item-done':'hideDetails',
-            'click .menu-item-button button' : 'onClickMenuItemButton'
-        },
+    events: {
+      'click .js-hotspotmenu-hotspot-btn':'showDetails',
+      'click .js-hotspotmenu-close-btn':'hideDetails',
+      'click .js-btn-click' : 'onClickMenuItemButton'
+    },
 
-        className: function() {
-            return [
-                'menu-item',
-                'menu-item-' + this.model.get('_id') ,
-                this.model.get('_classes'),
-                this.model.get('_isVisited') ? 'is-visited' : '',
-                this.model.get('_isComplete') ? 'is-complete' : '',
-                this.model.get('_isLocked') ? 'is-locked' : ''
-            ].join(' ');
-        },
+    preRender: function() {
+      this.listenTo(Adapt, {
+        'device:changed': this.deviceChanged,
+        'hotspotMenu:itemOpen': this.checkIfShouldClose
+      });
 
-        preRender: function() {
+      if (!this.model.get('_isVisited')) {
+        this.setVisitedIfBlocksComplete();
+      }
+      this.type = this.model.get('_hotspotMenuAudio')._hotspotMenuItem._type;
 
-          this.listenTo(Adapt, 'hotspotMenu:itemOpen', this.checkIfShouldClose);
-          this.listenTo(Adapt, 'device:changed', this.deviceChanged);
+      if (this.type == 'graphic') {
+        this.model.set('_graphicEnabled', true);
+      }
 
-          if (!this.model.get('_isVisited')) {
-            this.setVisitedIfBlocksComplete();
-          }
-          this.type = this.model.get('_hotspotMenuAudio')._hotspotMenuItem._type;
+      this.disableAnimation = Adapt.config.has('_disableAnimation') ? Adapt.config.get('_disableAnimation') : false;
+    },
 
-          if (this.type == 'graphic') {
-            this.model.set('_graphicEnabled', true);
-          }
+    postRender: function() {
+      this.setReadyStatus();
+      this.isPopupOpen = false;
 
-          this.disableAnimation = Adapt.config.has('_disableAnimation') ? Adapt.config.get('_disableAnimation') : false;
-        },
+      // Check for button type
+      switch (this.type) {
+        case 'text':
+          this.$('.hotspotmenu-item__hotspot').addClass('title-enabled');
+          this.$('.hotspotmenu-item__hotspot').html(this.model.get('_hotspotMenuAudio')._hotspotMenuItem.text);
+          break;
+        case 'number':
+          this.$('.hotspotmenu-item__hotspot').addClass('number-enabled');
+          this.$('.hotspotmenu-item__hotspot').html(this.model.get('_hotspotMenuAudio')._hotspotMenuItem.number);
+          break;
+        case 'icon':
+          this.$('.hotspotmenu-item__hotspot').addClass('icon-enabled icon '+this.model.get('_hotspotMenuAudio')._hotspotMenuItem._icon);
+          break;
+        case 'custom':
+          this.$('.hotspotmenu-item__hotspot').addClass('icon-enabled icon '+this.model.get('_hotspotMenuAudio')._hotspotMenuItem._custom);
+          break;
+      }
 
-        postRender: function() {
-            this.setReadyStatus();
-            this.isPopupOpen = false;
+      this.deviceChanged();
+    },
 
-            // Check for button type
-            switch (this.type) {
-              case 'text':
-                this.$('.menu-item-hotspot').addClass('title-enabled');
-                this.$('.menu-item-hotspot').html(this.model.get('_hotspotMenuAudio')._hotspotMenuItem.text);
-                break;
-              case 'number':
-                this.$('.menu-item-hotspot').addClass('number-enabled');
-                this.$('.menu-item-hotspot').html(this.model.get('_hotspotMenuAudio')._hotspotMenuItem.number);
-                break;
-              case 'icon':
-                this.$('.menu-item-hotspot').addClass('icon-enabled icon '+this.model.get('_hotspotMenuAudio')._hotspotMenuItem._icon);
-                break;
-              case 'custom':
-                this.$('.menu-item-hotspot').addClass('icon-enabled icon '+this.model.get('_hotspotMenuAudio')._hotspotMenuItem._custom);
-                break;
-            }
+    setVisitedIfBlocksComplete: function() {
+      var completedBlock = _.findWhere(this.model.findDescendantModels('components'), {_isComplete: true, _isOptional: false});
+      if (completedBlock != undefined) {
+        this.model.set('_isVisited', true);
+      }
+    },
 
-            this.deviceChanged();
-        },
+    showDetails: function(event) {
+      if (event) event.preventDefault();
 
-        setVisitedIfBlocksComplete: function() {
-            var completedBlock = _.findWhere(this.model.findDescendantModels('components'), {_isComplete: true, _isOptional: false});
-            if (completedBlock != undefined) {
-                this.model.set('_isVisited', true);
-            }
-        },
+      if (this.model.get('_hotspotMenuAudio')._bypassPopup) {
+        this.onClickMenuItemButton();
+      } else {
+        this.isPopupOpen = true;
+        var $element = $(event.currentTarget);
 
-        showDetails: function(event) {
-            if(event) event.preventDefault();
+        if (this.disableAnimation) {
+          this.$('.hotspotmenu-item__overlay').css('display', 'block');
+          this.$('.hotspotmenu-item__inner').addClass('show-item');
+        } else {
+          this.$('.hotspotmenu-item__overlay').velocity({ opacity: 0 }, {duration:0}).velocity({ opacity: 1 }, {duration:400, begin: _.bind(function() {
+            this.$('.hotspotmenu-item__overlay').css('display', 'block');
+          }, this)});
 
-            if(this.model.get('_hotspotMenuAudio')._bypassPopup) {
-              this.onClickMenuItemButton();
-            } else {
-              this.isPopupOpen = true;
-              var $element = $(event.currentTarget);
+          this.$('.hotspotmenu-item__inner').velocity({ opacity: 0 }, {duration:0}).velocity({ opacity: 1 }, {duration:400, begin: _.bind(function() {
+            this.$('.hotspotmenu-item__inner').addClass('show-item');
+          }, this)});
+        }
 
-              if (this.disableAnimation) {
-                  this.$('.menu-item-overlay').css('display', 'block');
-                  this.$('.menu-item-inner').addClass('show-item');
-              } else {
-                  this.$('.menu-item-overlay').velocity({ opacity: 0 }, {duration:0}).velocity({ opacity: 1 }, {duration:400, begin: _.bind(function() {
-                    this.$('.menu-item-overlay').css('display', 'block');
-                  }, this)});
-                  this.$('.menu-item-inner').velocity({ opacity: 0 }, {duration:0}).velocity({ opacity: 1 }, {duration:400, begin: _.bind(function() {
-                    this.$('.menu-item-inner').addClass('show-item');
-                  }, this)});
-              }
-
-              Adapt.trigger('hotspotMenu:itemOpen', $element.attr('data-id'));
-              // Audio
-              if(Adapt.audio.audioClip[this.model.get('_hotspotMenuAudio')._audio._channel].status==1){
-                  // Check if audio is set to autoplay
-                  if(this.model.get('_hotspotMenuAudio')._audio._isEnabled && this.model.get('_hotspotMenuAudio')._audio._autoplay){
-                      Adapt.trigger('audio:playAudio', this.model.get('_hotspotMenuAudio')._audio._media.src, this.model.get('_id'), this.model.get('_hotspotMenuAudio')._audio._channel);
-                  }
-              }
-              Adapt.trigger('popup:opened', this.$('.menu-item-inner'));
-              Adapt.a11y.focusFirst($('.menu-item-title-inner'));
-              this.$('.menu-item-overlay').on('click', _.bind(this.hideDetails, this));
-            }
-        },
-
-        hideDetails: function(event) {
-            if(event) event.preventDefault();
-            this.isPopupOpen = false;
-            this.$('.menu-item-inner').removeClass('show-item');
-            if(this.model.get('_hotspotMenuAudio')._audio._isEnabled){
-                Adapt.trigger('audio:pauseAudio', this.model.get('_hotspotMenuAudio')._audio._channel);
-            }
-            if (this.disableAnimation) {
-                this.$('.menu-item-overlay').css('display', 'none');
-            } else {
-                this.$('.menu-item-overlay').velocity({ opacity: 0 }, {duration:400, complete:_.bind(function() {
-                  this.$('.menu-item-overlay').css('display', 'none');
-                }, this)});
-            }
-            this.$('.menu-item-overlay').off('click');
-            Adapt.trigger('popup:closed', this.$('.menu-item-inner'));
-        },
-
-        checkIfShouldClose: function(id) {
-            if(this.model.get('_id') != id) {
-                this.hideDetails();
-            }
-        },
-
-        onClickMenuItemButton: function(event) {
-            if(event && event.preventDefault) event.preventDefault();
-            if(this.model.get('_isLocked')) return;
-
-            this.hideDetails();
-            Backbone.history.navigate('#/id/' + this.model.get('_id'), {trigger: true});
-        },
-
-        deviceChanged: function() {
-          if (Adapt.device.screenSize === 'large') {
-            this.$('.menu-item-hotspot').css({
-              'top': this.model.get('_hotspotMenuAudio')._hotspotMenuItem._position._top+'%',
-              'left': this.model.get('_hotspotMenuAudio')._hotspotMenuItem._position._left+'%'
-            });
-            this.$('.menu-item-graphic-button').css({
-              'top': this.model.get('_hotspotMenuAudio')._hotspotMenuItem._position._top+'%',
-              'left': this.model.get('_hotspotMenuAudio')._hotspotMenuItem._position._left+'%'
-            });
-          } else {
-            this.$('.menu-item-hotspot').css({
-              'top': '',
-              'left': ''
-            });
-            this.$('.menu-item-graphic-button').css({
-              'top': '',
-              'left': ''
-            });
+        Adapt.trigger('hotspotMenu:itemOpen', $element.attr('data-id'));
+        // Audio
+        if (Adapt.audio && Adapt.audio.audioClip[this.model.get('_hotspotMenuAudio')._audio._channel].status==1){
+          // Check if audio is set to autoplay
+          if (this.model.get('_hotspotMenuAudio')._audio._isEnabled && this.model.get('_hotspotMenuAudio')._audio._autoplay){
+            Adapt.trigger('audio:playAudio', this.model.get('_hotspotMenuAudio')._audio._media.src, this.model.get('_id'), this.model.get('_hotspotMenuAudio')._audio._channel);
           }
         }
 
-    }, {
-        template:'hotspot-menu-item'
-    });
+        Adapt.trigger('popup:opened', this.$('.hotspotmenu-item__inner'));
+        Adapt.a11y.focusFirst($('.hotspotmenu-item__title-inner'));
+        this.$('.hotspotmenu-item__overlay').on('click', _.bind(this.hideDetails, this));
+      }
+    },
 
-    return HotspotItemView;
+    hideDetails: function(event) {
+      if (event) event.preventDefault();
+
+      this.isPopupOpen = false;
+      this.$('.hotspotmenu-item__inner').removeClass('show-item');
+
+      if (this.model.get('_hotspotMenuAudio')._audio._isEnabled){
+        Adapt.trigger('audio:pauseAudio', this.model.get('_hotspotMenuAudio')._audio._channel);
+      }
+
+      if (this.disableAnimation) {
+        this.$('.hotspotmenu-item__overlay').css('display', 'none');
+      } else {
+        this.$('.hotspotmenu-item__overlay').velocity({ opacity: 0 }, {duration:400, complete:_.bind(function() {
+          this.$('.hotspotmenu-item__overlay').css('display', 'none');
+        }, this)});
+      }
+
+      this.$('.hotspotmenu-item__overlay').off('click');
+      Adapt.trigger('popup:closed', this.$('.hotspotmenu-item__inner'));
+    },
+
+    checkIfShouldClose: function(id) {
+      if (this.model.get('_id') != id) {
+        this.hideDetails();
+      }
+    },
+
+    onClickMenuItemButton: function(event) {
+      if (event && event.preventDefault) event.preventDefault();
+      if (this.model.get('_isLocked')) return;
+
+      this.hideDetails();
+      Backbone.history.navigate('#/id/' + this.model.get('_id'), {trigger: true});
+    },
+
+    deviceChanged: function() {
+      if (Adapt.device.screenSize === 'large') {
+        this.$('.hotspotmenu-item__hotspot').css({
+          'top': this.model.get('_hotspotMenuAudio')._hotspotMenuItem._position._top+'%',
+          'left': this.model.get('_hotspotMenuAudio')._hotspotMenuItem._position._left+'%'
+        });
+
+        this.$('.hotspotmenu-item__graphic-button').css({
+          'top': this.model.get('_hotspotMenuAudio')._hotspotMenuItem._position._top+'%',
+          'left': this.model.get('_hotspotMenuAudio')._hotspotMenuItem._position._left+'%'
+        });
+
+      } else {
+        this.$('.hotspotmenu-item__hotspot').css({
+          'top': '',
+          'left': ''
+        });
+
+        this.$('.hotspotmenu-item__graphic-button').css({
+          'top': '',
+          'left': ''
+        });
+      }
+    }
+
+  }, {
+    className: 'hotspotmenu-item',
+    template:'hotspot-menu-item'
+  });
+
+  return HotspotItemView;
 
 });
--- js/adapt-hotspot-menu.js
+++ js/adapt-hotspot-menu.js
@@ -7,7 +7,7 @@ define([
   var HotspotView = MenuView.extend({
 
     className: function() {
-      return MenuView.prototype.className.apply(this) + ' hotspot-menu';
+      return MenuView.prototype.className.apply(this) + ' hotspotmenu';
     },
 
     attributes: function() {
@@ -15,10 +15,6 @@ define([
         'role': 'main',
         'aria-labelledby': this.model.get('_id') + '-heading'
       }, this);
-    },
-
-    events: {
-      'click .audio-toggle': 'toggleAudio'
     },
 
     postRender: function() {
@@ -38,122 +34,37 @@ define([
 
       this.listenTo(Adapt, 'device:changed', this.deviceChanged);
       this.listenTo(Adapt, 'menuView:ready device:resize', this.deviceResize);
+      this.listenToOnce(Adapt, 'remove', this.removeListeners);
 
       this.deviceChanged();
 
-      this.$('.hotspot-container-inner').imageready(function() {
+      this.$('.hotspotmenu-container__inner').imageready(function() {
         this.deviceResize();
       }.bind(this));
 
-      if (this.model.get('_hotspotMenuAudio')._audio && this.model.get('_hotspotMenuAudio')._audio._isEnabled) {
-        this.listenTo(Adapt, 'audio:updateAudioStatus', this.updateToggle);
-        this.listenToOnce(Adapt, 'remove', this.removeListeners);
-        this.setupAudio();
-      }
-
-      this.startWidth = this.$('.hotspot-container-image').outerWidth();
-    },
-
-    setupAudio: function() {
-      this.audioChannel = this.model.get('_hotspotMenuAudio')._audio._channel;
-      this.elementId = this.model.get('_id');
-      this.audioFile = this.model.get('_hotspotMenuAudio')._audio._media.src;
-      Adapt.audio.audioClip[this.audioChannel].newID = this.elementId;
-      this.audioIcon = Adapt.audio.iconPlay;
-      this.pausedTime = '';
-
-      // Autoplay
-      if (Adapt.audio.autoPlayGlobal || this.model.get('_hotspotMenuAudio')._audio._autoplay) {
-        this.canAutoplay = true;
-      } else {
-        this.canAutoplay = false;
-      }
-
-      // Add audio icon
-      this.$('.audio-toggle').addClass(this.audioIcon);
-
-      // Hide controls if set in JSON or if audio is turned off
-      if (this.model.get('_hotspotMenuAudio')._audio._showControls == false || Adapt.audio.audioClip[this.audioChannel].status == 0) {
-        this.$('.audio-inner button').hide();
-      }
-
-      // Set listener for when clip ends
-      $(Adapt.audio.audioClip[this.audioChannel]).on('ended', _.bind(this.onAudioEnded, this));
-
-      // Play audio if autoplay is true
-      if (this.canAutoplay) {
-        // Check if audio is set to on
-        if (Adapt.audio.audioClip[this.audioChannel].status == 1) {
-          Adapt.trigger('audio:playAudio', this.audioFile, this.elementId, this.audioChannel);
-        }
-      }
-    },
-
-    onAudioEnded: function() {
-      Adapt.trigger('audio:audioEnded', this.audioChannel);
-    },
-
-    toggleAudio: function(event) {
-      if (event) event.preventDefault();
-      Adapt.audio.audioClip[this.audioChannel].onscreenID = '';
-      if ($(event.currentTarget).hasClass('playing')) {
-        this.pauseAudio();
-      } else {
-        this.playAudio();
-      }
-    },
-
-    playAudio: function() {
-      if (Adapt.audio.pauseStopAction == 'pause') {
-        Adapt.audio.audioClip[this.audioChannel].play(this.pausedTime);
-        this.$('.audio-toggle').removeClass(Adapt.audio.iconPlay);
-        this.$('.audio-toggle').addClass(Adapt.audio.iconPause);
-        this.$('.audio-toggle').addClass('playing');
-      } else {
-        Adapt.trigger('audio:playAudio', this.audioFile, this.elementId, this.audioChannel);
-      }
-    },
-
-    pauseAudio: function() {
-      if (Adapt.audio.pauseStopAction == 'pause') {
-        this.pausedTime = Adapt.audio.audioClip[this.audioChannel].currentTime;
-        Adapt.audio.audioClip[this.audioChannel].pause();
-        this.$('.audio-toggle').removeClass(Adapt.audio.iconPause);
-        this.$('.audio-toggle').addClass(Adapt.audio.iconPlay);
-        this.$('.audio-toggle').removeClass('playing');
-      } else {
-        Adapt.trigger('audio:pauseAudio', this.audioChannel);
-      }
-    },
-
-    updateToggle: function() {
-      if (Adapt.audio.audioStatus == 1 && this.model.get('_hotspotMenuAudio')._audio._showControls == true) {
-        this.$('.audio-toggle').removeClass('hidden');
-      } else {
-        this.$('.audio-toggle').addClass('hidden');
-      }
+      this.startWidth = this.$('.hotspotmenu-container__image').outerWidth();
     },
 
     deviceChanged: function() {
-      this.$('.menu-item-overlay').css('display', 'none');
+      this.$('.menu-item__overlay').css('display', 'none');
       if (Adapt.device.screenSize === 'large') {
-        this.$('.menu-item-inner').removeClass('show-item');
+        this.$('.menu-item__inner').removeClass('show-item');
         if (this.model.get('_hotspotMenuAudio').instruction !== '') {
-          this.$('.menu-instruction-inner').html(this.model.get('_hotspotMenuAudio').instruction);
+          this.$('.menu__instruction-inner').html(this.model.get('_hotspotMenuAudio').instruction);
         }
       } else {
         if (this.model.get('_hotspotMenuAudio').instructionMobile !== '') {
-          this.$('.menu-instruction-inner').html(this.model.get('_hotspotMenuAudio').instructionMobile);
+          this.$('.menu__instruction-inner').html(this.model.get('_hotspotMenuAudio').instructionMobile);
         }
       }
     },
 
     deviceResize: function() {
-      var menuWidth = this.$('.hotspot-container-image').outerWidth();
-      var menuHeight = this.$('.hotspot-container-inner').outerHeight();
+      var menuWidth = this.$('.hotspotmenu-container__image').outerWidth();
+      var menuHeight = this.$('.hotspotmenu-container__inner').outerHeight();
 
       var navigationHeight = $('.nav').outerHeight();
-      var headerHeight = this.$('.menu-header').outerHeight();
+      var headerHeight = this.$('.menu__header').outerHeight();
 
       var decrease = this.startWidth - menuWidth;
       var percentageChanged = Math.round((decrease / this.startWidth) * 100);
@@ -182,7 +93,6 @@ define([
     removeListeners: function() {
       this.stopListening(Adapt, 'device:changed', this.deviceChanged);
       this.stopListening(Adapt, 'device:resize', this.deviceResize);
-      Adapt.trigger('audio:pauseAudio', this.audioChannel);
     }
 
   }, {
